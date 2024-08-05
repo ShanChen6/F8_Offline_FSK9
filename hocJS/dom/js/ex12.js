5c862103b1552df8d48a35736cc34868eaece1aa
ShanDev <shanchen611@gmail.com>
2024-08-05 19:17:51 +0700

    update code day 05082024

---
+++ hocJS/dom/js/ex12.js
@@ -0,0 +1,11 @@
+var root = document.querySelector("#root");
+
+// Tao element
+var h1 = document.createElement("h1");
+
+h1.innerText = `Hoc JS khong kho`;
+h1.id = "title";
+h1.className = "title";
+// console.log(h1);
+
+root.append(h1);
