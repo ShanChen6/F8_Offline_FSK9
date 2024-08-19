b0d8241173d358943395edc5b779e01e0a78df0d
ShanDev <shanchen611@gmail.com>
2024-08-19 22:10:31 +0700

    update code day 19082024

--- hocJS/dom/js/ex22.js
+++ hocJS/dom/js/ex22.js
@@ -4,11 +4,25 @@ if (el) {
   if (dataValue) {
     var func = new Function(`return ${dataValue}`);
     var data = func();
-    for (var key in data) {
-      var outputEl = document.querySelector(`[x-text="${key}"]`);
-      if (outputEl) {
-        outputEl.innerText = data[key];
+    var render = function () {
+      for (var key in data) {
+        var outputEl = document.querySelector(`[x-text="${key}"]`);
+        if (outputEl) {
+          outputEl.innerText = data[key];
+        }
       }
+    };
+    render();
+
+    var eventEl = document.querySelector(`[click]`);
+    if (eventEl) {
+      var eventValue = eventEl.getAttribute("click");
+      eventEl.addEventListener("click", function () {
+        var func = new Function(`data.` + eventValue);
+        func();
+        render();
+        // console.log(`data.` + eventValue);
+      });
     }
   }
 }
